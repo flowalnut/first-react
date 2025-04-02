9386290a75fd6e2a74d160f5e246dbe068cef46d
jieun cha <flowalnut@gmail.com>
2025-04-03 01:43:10 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import {useState} from "react"//4-1.스테이트를 가져오는 리액트의 �
 import './App.css';
 import Box from "./component/Box"
 
+
+
 //선생님 로직.
 //1. 박스 2개 (타이틀, 사진, 결과)
 //2. 가위바위보 버튼 3개 
 //3. 버튼을 클릭하면 클릭한 값이 박스에 보임>onclick>우리가 실행시키고픈 함수play를 {}에 담은 후, app안에 만들기
 //4. 컴퓨터는 랜덤하게 아이템 선택이 된다(중요point: ui를 바꾸게 할 때는 state를 만들어야한다.)
-//5. 3,4번의 결과를 가지고 누가 이겼는지 승패를 따진다. 
+//5. 3,4번의 결과를 가지고 누가 이겼는지 승패를 따진다. 나는 내가선택한값/컴퓨터가선택한값을 알 수 있따. 승패를 보여주는 state를 만들어야함.go app()
 //6. 승패결과에 따라 테두리색이 변경(지면-빨강, 이기면-초록색, 비기면-검은색)
+
+
+ //4-22. 객체를 배열화 시작할 것임. randomChoice로go. 
 
 const choice = {//바뀌지 x const, 초이스에 어떤게 있는지, 객체를 만들어본다.쉼표필수.
   rock:{
@@ -29,6 +34,14 @@ function App() {
   const [userSelect, setUserSelect] = useState (null)//4-3. state에 있는 값이 변할 때 마다, ui가 변할 예정. 
   //4-4. 이제 우리가 해줘야하는 것은 useSelect에 값은 바꿔줘야 한다. 내가 선택한 값으로!>>play함수로 go
 
+  //4-17.컴퓨터를 위한 ui>>state를 만들어줘야한다. 그리고 이 컴퓨터셀렉트는 어디서 보여줄까?>> 밑의 <Box />에서! (커멘드 풀어주기) 이제 컴퓨터에도 아이템속성이 들어감.
+  const [computerSelect, setComputerSelect] = useState (null)
+  //4-18. 이제 유저가 선택할 '때', 컴퓨터가 랜덤한 값을 선택하도록 만들어야한다. 그 순간이 언제인가? play () 함수!play함수 안에서 컴퓨터가 값을 랜덤하게 선택하도록 할것임.   
+
+  const [result, setResult] = useState ("") //5-1. 여기에는 비어있는 스트링타입을 넣어준다. > result의 값을 밑의 <Box />에 보내준다.
+  //5-2. Box.js컴포넌트로 돌아가 WIN대신 다이나믹한 값인 props.result로 바꾸기>play함수에서 세팅해줘야함 go
+
+
   const play = (userChoice) => {
     console.log("선택 됨!", userChoice)
     //3-1. 여기까지, onclick이 동작함, but 내가 무엇을 선택했는지는 나오지x
@@ -45,13 +58,76 @@ function App() {
     setUserSelect(choice[userChoice])
     //4-7. 이제 버튼에다가 넘겨줄 것. 가서 item = {userSelect}내가 선택한 값은 이것이다. 아이템에게 알려주기.
     //4-8. 이제 아이템을 이용하면 됨. box.js 가기
+
+
+    //4-19. 컴퓨터가 랜덤하게 값을 선택한다는 함수를 만들어주자. 
+    let computerChoice = randomChoice() //랜덤초이스의 결과값이 컴퓨터초이스에 담긴다. //4-29.randomChoice가 리턴이 되면서 컴퓨터초이스에 들어가게된다.
+    setComputerSelect(computerChoice) 
+    //4-30.setComputerSelect. computerItem을 초기화 시켜줄게요라는 뜻.>무엇으로?computerChoice의 값으로...
+    //주의! randomChoice()가 이미 choice 객체 중 하나를 리턴하므로, choice[computerChoice]를 다시 접근할 필요는 없다! 4끝.
+
+    //5-3.판단하는 함수 만들기. 두가지값(유저가선택한 값/컴퓨터가 선택한 값)
+    setResult(judgement(choice[userChoice], computerChoice));
+    //5-7.setResult()에 judgment의 리턴값judgement(choice[userChoice], computerChoice);을 넣어줄 수 있다.
+    //5-8. 그러나 지금 user입장에서 쓰인 함수만 있기 때문에 유저가 이겼는지 졌는지만 나오는 상황.
+    
+  }
+
+
+
+    //5-4.이제 함수를 만들고, 두개의 매개변수 이름입력. 
+  const judgement = (user, computer) => {
+    console.log("유저", user, "computer", computer)
+    //5-5. 이제부터 로직을 생각해보기
+    //유저 == 컴퓨터 tie
+    //유저 == 바위, 컴퓨터 == 가위  유저가 이김
+    //유저 == 바위, 컴퓨터 == 보    유저가 짐
+    //유저 == 가위, 컴퓨터 == 보    유저가 이김
+    //유저 == 가위, 컴퓨터 == 바위  유저가 짐
+    //유저 == 보,   컴퓨터 == 바위  유저가 이김
+    //유저 == 보,   컴퓨터 == 가위   유저가 짐
+    //오브젝트 안에서 name만 가지고 비교시작해야한다.삼항연산식 사용.
+    if(user.name === computer.name) {
+      return "tie"
+    } else if (user.name === "Rock") 
+      return computer.name === "Scissor"? "win" : "lose"
+    else if (user.name === "Scissor") 
+      return computer.name === "Paper"? "win" : "lose"
+    else if (user.name === "Paper") 
+      return computer.name === "Rock"? "win" : "lose"
+    //5-6.이제 const judgment가 스트링값을 리턴하게 되었다. judgement함수로 go.
+  }
+
+
+    //4-20. 이제 랜덤초이스라는 함수를 만들어야 함. 랜덤으로 초이스하게 어떻게 할까?
+    /*
+    4-21. Math.random 0~1사이 값 랜덤으로 추출. 이걸 어떻게 choice랑 연결할 것인가.
+          우린 숫자만 가져올 수 o. 아이템과 숫자를 연결짓는 방법=배열.
+          배열에 아이템이 들어가있으면 > 아이템 각각에 인덱스 자동 부여가 됨. [item, item, item] = [0, 1, 2]
+          인덱스 번호 = 숫자. >> math.random을 통해서 가져올 수 o. 
+          이 값을 통해서 choice아래에있는 갖고올 수 o. choice로 이동!
+    */
+  
+  const randomChoice = () => {
+    //4-23. Object.keys >> 배열화하는 중.. >> 객체(Rock, Scissor, Paper)에서 키값만 뽑아서 어레이로 만들어주는 함수. 
+    //4-24. 어레이에는 인덱스 번호가 o > 인덱스 번호 0,1,2중에 하나만 랜덤으로 받아오면 됨. 
+    let itemArray = Object.keys(choice);
+    console.log("아이템어레이는", itemArray)
+
+    //4-25. 랜덤함수에 *itemArray.length; 곱해주고, 소수점 제거 >> 0, 1, 2가 나오게 될 것. 
+    let randomItem = Math.floor(Math.random()*itemArray.length)
+    console.log("randomItem", randomItem)
+    let final = itemArray[randomItem]//4-26.이제 파이널 값을 담을 변수 만들고 > 아이템 어레이에 랜덤아이템의 인덱스 번호 받은것을 넣어준다.
+    console.log("final은",final)//4-27.이제 인덱스 번호에 맞춰서 아이템이 반환이 된다.
+
+    return choice[final];//4-28.리턴이 되면서 위의 컴퓨터초이스에 들어가게된다. go!
   }
 
   return (
     <div>
       <div class="main">
-        <Box title="YOU" item = {userSelect}/>
-        {/* <Box title="COMPUTER"/> */}
+        <Box title="YOU" item = {userSelect} result = {result}/>
+        <Box title="COMPUTER" item = {computerSelect} result = {result}/>
       </div>
       <div class="main two">
         <button onClick = {() => play("scissor")}>가위</button>
--- src/component/Box.js
+++ src/component/Box.js
@@ -7,13 +7,33 @@ const Box = (props) => {
   //4-12. 에러 고치려면, props.item && 입력. 이것을 가드라고 함.null은 false가 자동리턴되기 때문.
   //4-13. props.item이 실행되지 못하면 뒤의  props.item.img도 실행 x. 
   //4-14. 유저가 넘겨준 아이템값이 있으면, true가 되면서 뒤의 아이템의 이미지값도 불러오게 됨. 리액트는 가드값을 넣어줘야한다.
-  //4-15. useSelect는 다이나믹한 값. 내가 선택하기 전까지는 null임. 아예 값이 없는 것. 따라서 상황에 맞춰서 에러 방지위해 가드 넣어줘야함. 
+  //4-15. useSelect는 다이나믹한 값. 내가 선택하기 전까지는 null임. 아예 값이 없는 것. 따라서 상황에 맞춰서 에러 방지위해 가드 넣어줘야함.
+  //4-16. 이제 컴퓨터를 위한 ui를 시작해야한다. app.js로 이동! 
+
+  //숙제1. 이기고 지고 반대로 하는 것. 
+  //숙제2. 승패 결과에따라 테두리 색이 달라지는 것.
+  let result; 
+
+  if(
+    props.title === "COMPUTER" &&
+    props.result !== "tie" &&
+    props.result !== "" ) { //만약 카드 타이틀일 경우, 비기지 않았을경우, 비어있지 않을 경우.
+      result = props.result === "win" ? "lose" : "win"; //아래 저지먼트값으로 result가 win일경우 컴퓨터 카드값은 lose, 아니면 win이 된다.
+    } else {
+      result = props.result; //위의 경우가 아니라면, 전달된 결과 그대로를 쓴다.
+    }
+
+    let img = props.item
+    ? props.item.img
+    : "https://plus.unsplash.com/premium_photo-1680303134459-912abf8efe2f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8JTNGfGVufDB8fDB8fHww"
+
   return (
-    <div>
-      <div class="Box">
+    <div class="main_box">
+      <div className={`box ${result}`}>
         <h1>{props.title}</h1>
-        <img class="item_img" src={props.item && props.item.img}/>
-        <h2>WIN</h2>
+        <h2>{props.item && props.item.name}</h2>
+        <figure><img className="item_img" src={img}/></figure>
+        <h2>{result}</h2>
       </div>
     </div>
   )
